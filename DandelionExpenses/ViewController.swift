fa1c456ad52d2a7c3e23a9b65fbdf21741412d49
OscarYen <oscaryen@sndboxsmart.com>
2021-12-13 02:26:30 +0800

    Initial Commit

---
+++ DandelionExpenses/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DandelionExpenses
+//
+//  Created by OscarYen on 2021/12/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
