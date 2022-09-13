f5aa519ace3582e1c1799ffd68edbca9092ef0c0
OscarYen <oscaryen@sndboxsmart.com>
2022-09-13 09:50:59 +0800

    add textview in edit

--- DandelionExpenses/View/Dlailog/ScheduleEditDlg.swift
+++ DandelionExpenses/View/Dlailog/ScheduleEditDlg.swift
@@ -13,12 +13,13 @@ protocol ScheduleEditDlgDelegate {
 }
 
 
-class ScheduleEditDlg: UIViewController, UITextViewDelegate, NSTextStorageDelegate {
+class ScheduleEditDlg: UIViewController, UITextViewDelegate {
 
     @IBOutlet weak var txtEvent: UITextView!
     @IBOutlet weak var txtNote: UITextField!
     @IBOutlet weak var btnCancel: UIButton!
     @IBOutlet weak var btnOk: UIButton!
+    @IBOutlet weak var textView: UIView!
     
     var _delegate: ScheduleEditDlgDelegate?
     
@@ -43,6 +44,40 @@ class ScheduleEditDlg: UIViewController, UITextViewDelegate, NSTextStorageDelega
             
             break
         }
+    }
+    
+    //=========================================================================
+    // MARK: - txtNote Listener
+    //=========================================================================
+    func textViewDidBeginEditing(_ textView: UITextView) {
+       
+        if txtNote.textColor == UIColor.lightGray {
+            txtNote.placeholder = nil
+            txtNote.textColor = UIColor.black
+        
+        }
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+       
+        if textView.text.count == 0 {
+            txtNote.placeholder = NSLocalizedString("note", comment: "")
+        }
+        else {
+            txtNote.placeholder =  ""
+            
+            
+        }
+        textView.borderColor = UIColor.lightGray
+        textView.borderWidth = 0
+    }
+    
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+//        if text == "\n" {
+//            txtNote.resignFirstResponder()
+//            return false
+//        }
+        return true
     }
 
 }
