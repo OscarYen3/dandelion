971c60daddd2bf605b47c6f28595570204a4fb36
oscaryen <mapd24th3ee@gmail.com>
2024-08-30 11:06:45 +0800

    優化說明頁面

--- DandelionExpenses/View/Dlailog/QuestionDlg.swift
+++ DandelionExpenses/View/Dlailog/QuestionDlg.swift
@@ -12,8 +12,15 @@ class QuestionDlg: UIViewController {
     @IBOutlet weak var lblInfo: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        lblInfo.text = "本帳本計算方式為所有人會透過公費去做收入與支出，建議不要直接轉給墊錢的人，統一由公費管帳。透過公費領錢。\n\n 最後帳目會以無條件進入法作結算，把誤差降到最低。"
+        
+        lblInfo.text =
+        """
+         1.本帳本計算方式為所有人會透過公費去做收入與支出，建議不要直接轉給墊錢的人，統一由公費管帳。透過公費領錢。\n
+         2.最後帳目會以無條件進入法作結算，把誤差降到最低。\n
+         3.在人物清單中，所有人員負債為零時，才是帳務結清。\n
+         4.若採買中，還有區分分攤人員，建議拆出來做人員分攤。（例如酒類或是額外添購）\n
+         5.結清賬務後，若要繼續記帳，建議另開帳本作下次公費計算。(因為明細會持續存在)\n
+        """
     }
 
     @IBAction func ok(_ sender: Any) {
